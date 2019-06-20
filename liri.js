f54a551e81086c63f5dad7a024707f8a9733e3c6
TJ <tjames02@hamline.edu>
2019-06-19 23:01:50 -0500

    spotify fix

--- liri.js
+++ liri.js
@@ -1,14 +1,15 @@
 // At the top of the liri.js file, add code to read and set any environment variables with the dotenv package:
 
 
-require("dotenv").config();
+require("dotenv").config("./*.env");
 
 // Add the code required to import the keys.js file and store it in a variable.
 
 var keys = require("./keys");
 
 // You should then be able to access your keys information like so:
-// var spotify = new Spotify(keys.spotify);
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 
 //First Command: node liri.js concert-this <artist/band name here>
 
@@ -18,6 +19,9 @@ var Band = require("./bands");
 
 // Create a new Band object
 var band = new Band();
+
+var Movie = require("./movie");
+var movie = new Movie();
 
 // Grab search command line argument
 var command = process.argv[2];
@@ -38,13 +42,20 @@ if (!term) {
 if (command === "concert-this") {
   console.log("Searching the Bands in Town Artist Events API");
   band.findConcert(term);
+}else if (command === "spotify-this-song") {
+  console.log("Searching Spotify for information about a song");
+  spotify.search({ type: 'track', query: term }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   
+  console.log(data); 
+  });
+}else if (command === "movie-this"){
+    console.log ("Searching the OMBD API for information about movie")
+    movie.findMovie(term);
 }
-// } else if (command === "spotify-this-song") {
-//   tv.findActor(term);
-//   console.log("Searching Spotify for information about a song");
-// }else if (command === "movie-this"){
-//     console.log ("Searching the OMBD API for information about movie")
-// }else if (command === "do-what-it-says"){
+// else if (command === "do-what-it-says"){
 //     console.log ("Searching for random.txt")
 // }
 // Name of the venue
@@ -54,4 +65,5 @@ if (command === "concert-this") {
 
 
 // Date of the Event (use moment to format this as "MM/DD/YYYY")
+
 
